6e3df29b42eb07d9c48226dd77b0ea47ef5ef2a9
Ben Hasselgren <ben.nadalito@gmail.com>
2018-06-01 15:43:10 +0000

    started testing bug

--- bugs/urls.py
+++ bugs/urls.py
@@ -3,7 +3,7 @@ from .views import all_bugs, bug_detail, add_or_edit_bug, upvote_bug
 
 urlpatterns = [
     url(r'^$', all_bugs, name='bugs'),
-    url(r'^(?P<pk>\d+)/$', upvote_bug, name='upvote_bug'),
-    url(r'^upvote(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^upvote(?P<pk>\d+)/$', upvote_bug, name='upvote_bug'),
     url(r'^new/$', add_or_edit_bug, name='new_bug'),
 ]
