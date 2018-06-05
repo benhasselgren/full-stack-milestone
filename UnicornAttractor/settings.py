7effd99ed3bd152a8355efb75fce8ac65b03aa64
Ben Hasselgren <ben.nadalito@gmail.com>
2018-06-05 16:17:13 +0000

    updated database settings

--- UnicornAttractor/settings.py
+++ UnicornAttractor/settings.py
@@ -90,7 +90,16 @@ WSGI_APPLICATION = 'UnicornAttractor.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+if "DATABASE_URL" in  os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Databse url not found. Using Sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }   
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
