96bcd9270f5e38873c80e8bba3ee4b8fd0186dfc
Ben Hasselgren <ben.nadalito@gmail.com>
2018-06-05 17:05:23 +0000

    added whitenoise

--- UnicornAttractor/settings.py
+++ UnicornAttractor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'UnicornAttractor.urls'
